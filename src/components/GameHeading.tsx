fa6a59f638fedd503744a8f5e15a385810ea9d5e
Akash Padampalle <Akashpadampalle780@gmail.com>
2024-05-20 18:26:16 +0530

    Update GameHeading.tsx

--- src/components/GameHeading.tsx
+++ src/components/GameHeading.tsx
@@ -8,7 +8,7 @@ interface Props {
 const GameHeading = ({ gameQuery }: Props) => {
   const heading = `${gameQuery.platform?.name || ""} ${
     gameQuery.genre?.name || ""
-  } Games Games;
+  } Games Games`;
 
   return (
     <Heading as="h1" marginY={5}>
